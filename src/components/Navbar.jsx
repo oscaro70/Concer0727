5151c1b08438c062ef99272b1638124d32711f77
oscaro70 <oscar_o_a@hotmail.com>
2025-11-11 14:45:51 -0600

    Change 'Sobre nosotros' to 'Nosotros' in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/about">Sobre nosotros</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about">Nosotros</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/mision">Misión y visión</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/productos">Productos</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/clientes">Clientes</Link></li>
